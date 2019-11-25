6fdfd0ee49fbdccde9298cee3f004ee565a489dd
Igor Stojković <igors@nordeus.com>
2019-11-25 10:54:44 +0100

    Displaying a special icon in project window when there are symbolic links

--- Assets/NuGet/Editor/SystemProxy.cs
+++ Assets/NuGet/Editor/SystemProxy.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using Nordeus.Nuget.Utility;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -21,6 +23,7 @@ namespace NugetForUnity
 				// restore packages - this will be called EVERY time the project is loaded or a code-file changes.
 				NugetHelper.Restore();
 			}
+			EditorApplication.projectWindowItemOnGUI += ReplaceFolderIcon;
 		}
 
 		public static string CurrentDir => Application.dataPath;
@@ -182,6 +185,53 @@ namespace NugetForUnity
 			}
 
 			return sBuilder.ToString();
+		}
+
+		private static readonly HashSet<string> pathsWithLinks = new HashSet<string>();
+		private static bool arePathsInitialized;
+		private static Texture2D linkIcon;
+
+		private static void ReplaceFolderIcon(string guid, Rect rect)
+		{
+			if (!arePathsInitialized)
+			{
+				arePathsInitialized = true;
+				var repoAbsPath = NugetHelper.NugetConfigFile.RepositoryPath.Replace("\\", "/");
+				var assetsIndex = repoAbsPath.IndexOf("/Assets/", StringComparison.Ordinal);
+				if (assetsIndex < 0) return;
+				assetsIndex++;
+				foreach (var dir in Directory.EnumerateDirectories(repoAbsPath))
+				{
+					if (!SymbolicLink.Exists(dir)) continue;
+					var g = AssetDatabase.AssetPathToGUID(dir.Substring(assetsIndex));
+					if (!string.IsNullOrEmpty(g)) pathsWithLinks.Add(g);
+				}
+
+				if (pathsWithLinks.Count > 0)
+				{
+					var repoPath = repoAbsPath.Substring(assetsIndex).Trim('/');
+					while (repoPath != null && repoPath != "Assets")
+					{
+						var g = AssetDatabase.AssetPathToGUID(repoPath);
+						if (!string.IsNullOrEmpty(g)) pathsWithLinks.Add(g);
+						repoPath = Path.GetDirectoryName(repoPath);
+					}
+				}
+			}
+			
+			if (!pathsWithLinks.Contains(guid)) return;
+			
+			if (linkIcon == null) linkIcon = Resources.Load<Texture2D>("link16");
+			if (linkIcon == null) return;
+
+			var smaller = rect.width < rect.height ? rect.width : rect.height;
+			var d = smaller > 16 ? 16 : 10;
+
+			if (smaller > 16) smaller = smaller / 1.6f + 10;
+			
+			rect = new Rect(rect.x + smaller - d + 2, rect.y + smaller - d, d, d);
+
+			GUI.DrawTexture(rect, linkIcon);
 		}
 	}
 }
